9fb3db51d13439ea7a8024e863f2826e155271d1
EiNSTeiN- <einstein@g3nius.org>
2023-05-01 22:41:06 -0400

    Use .stl for plates since previewing them work better, and the output does not change every time the files are re-generated, and add bed size so this works correctly for 350mm bed

--- plate.py
+++ plate.py
@@ -6,7 +6,7 @@ import re
 import glob
 
 slicer_bin = '/home/einstein/PrusaSlicer-2.5.0+linux-x64-GTK3-202209060725.AppImage'
-options = '--export-3mf --repair --output %s %s --merge %s'
+options = '--export-stl --ensure-on-bed --align-xy 0,0 --center 175,175 --repair --output %s %s --merge %s'
 
 plates = [
     ### Extruder
@@ -21,7 +21,7 @@ plates = [
             'STLs/Extruder/sb_adxl_mount_generic_15.5mm_c_c.stl',
             { 'STLs/Extruder/sb_adxl_washer_x2.stl': 2 },
         ],
-        output = "Plate/extruder_phaetus_dragon.3mf"
+        output = "Plate/extruder_phaetus_dragon.stl"
     ),
     dict(
         source = [
@@ -34,22 +34,22 @@ plates = [
             'STLs/Extruder/sb_adxl_mount_generic_15.5mm_c_c.stl',
             { 'STLs/Extruder/sb_adxl_washer_x2.stl': 2 },
         ],
-        output = "Plate/extruder_revo_voron.3mf"
+        output = "Plate/extruder_revo_voron.stl"
     ),
     dict(
         source = [
             'STLs/Extruder/[a]_*.stl',
             'STLs/Extruder/Cam_mount_SB.stl',
         ],
-        output = "Plate/extruder_[a].3mf"
+        output = "Plate/extruder_[a].stl"
     ),
     dict(
         source = ['STLs/Extruder/[c]_*.stl'],
-        output = "Plate/extruder_[c].3mf"
+        output = "Plate/extruder_[c].stl"
     ),
     dict(
         source = ['STLs/Extruder/[o]_*.stl'],
-        output = "Plate/extruder_[o].3mf"
+        output = "Plate/extruder_[o].stl"
     ),
 
     ### Gantry
@@ -60,45 +60,39 @@ plates = [
             { 'STLs/Gantry/[a]_z_chain_retainer_bracket_x2.stl': 2 },
             { 'STLs/Gantry/[a]_z_belt_clip_{lower,upper}_x4.stl': 4 },
         ],
-        output = "Plate/gantry_[a].3mf"
+        output = "Plate/gantry_[a].stl"
     ),
     dict(
         source = [
-            'STLs/Gantry/a_drive_frame_{lower,upper}.stl',
+            'STLs/Gantry/{a,b}_drive_frame_{lower,upper}.stl',
         ],
-        output = "Plate/gantry_a_drive.3mf"
-    ),
-    dict(
-        source = [
-            'STLs/Gantry/b_drive_frame_{lower,upper}.stl',
-        ],
-        output = "Plate/gantry_b_drive.3mf"
+        output = "Plate/gantry_ab_drive.stl"
     ),
     dict(
         source = [
             'STLs/Gantry/front_idler_{left,right}_{lower,upper}.stl',
         ],
-        output = "Plate/gantry_idlers.3mf"
+        output = "Plate/gantry_idlers.stl"
     ),
     dict(
         source = [
             'STLs/Gantry/Tap_*.stl',
             'STLs/Gantry/tap_*.stl',
         ],
-        output = "Plate/gantry_tap.3mf"
+        output = "Plate/gantry_tap.stl"
     ),
     dict(
         source = [
             'STLs/Gantry/xy_joint_{left,right}_{lower,upper}_MGN12.stl',
         ],
-        output = "Plate/gantry_xy_joints.3mf"
+        output = "Plate/gantry_xy_joints.stl"
     ),
     dict(
         source = [
             { 'STLs/Gantry/z_joint_{lower,upper}_x4.stl': 4 },
             'STLs/Gantry/z_chain_{bottom_anchor,guide}.stl',
         ],
-        output = "Plate/gantry_z.3mf"
+        output = "Plate/gantry_z.stl"
     ),
     
     ### Z Axis
@@ -106,21 +100,21 @@ plates = [
         source = [
             { 'STLs/Z_Drive/z_drive_{main,retainer}_{a,b}_x2.stl': 2 },
         ],
-        output = "Plate/z_axis_1.3mf"
+        output = "Plate/z_axis_1.stl"
     ),
     dict(
         source = [
             { 'STLs/Z_Idlers/z_tensioner_bracket_{a,b}_x2.stl': 2 },
             { 'STLs/Z_Drive/z_motor_mount_{a,b}_x2.stl': 2 },
         ],
-        output = "Plate/z_axis_2.3mf"
+        output = "Plate/z_axis_2.stl"
     ),
     dict(
         source = [
             { 'STLs/Z_Drive/[a]_*_x2.stl': 2 },
             { 'STLs/Z_Idlers/[a]_*_x4_*.stl': 4 },
         ],
-        output = "Plate/z_axis_[a].3mf"
+        output = "Plate/z_axis_[a].stl"
     ),
 
     ### Skirts
@@ -131,21 +125,21 @@ plates = [
             'STLs/Skirts/[a]_btt_knob_light_shield.stl',
             'STLs/Skirts/[a]_mini12864_case_{front_insert,hinge}.stl',
         ],
-        output = "Plate/skirts_[a].3mf"
+        output = "Plate/skirts_[a].stl"
     ),
     dict(
         source = [
             'STLs/Skirts/front_skirt_{a,b}_350.stl',
             'STLs/Skirts/mini12864_case_{front,rear}.stl',
         ],
-        output = "Plate/skirts_front.3mf"
+        output = "Plate/skirts_front.stl"
     ),
     dict(
         source = [
             { 'STLs/Skirts/side_skirt_{a,b}_350_x2.stl': 2},
             { 'STLs/Skirts/side_fan_support_x2.stl': 2},
         ],
-        output = "Plate/skirts_side.3mf"
+        output = "Plate/skirts_side.stl"
     ),
     dict(
         source = [
@@ -153,7 +147,7 @@ plates = [
             'STLs/Skirts/rear_center_skirt_350.stl',
             'STLs/Skirts/keystone_panel.stl',
         ],
-        output = "Plate/skirts_rear.3mf"
+        output = "Plate/skirts_rear.stl"
     ),
 
     ### Panels
@@ -163,14 +157,14 @@ plates = [
             'STLs/Panels/[a]_exhaust_fan_grill.stl',
             'STLs/Panels/[a]_filter_acess_cover.stl',
         ],
-        output = "Plate/panels_[a].3mf"
+        output = "Plate/panels_[a].stl"
     ),
     dict(
         source = [
             { 'STLs/Panels/bottom_panel_clip_x4.stl': 4 },
             { 'STLs/Panels/bottom_panel_hinge_x2.stl': 2 },
         ],
-        output = "Plate/panels_bottom.3mf"
+        output = "Plate/panels_bottom.stl"
     ),
     dict(
         source = [
@@ -179,7 +173,7 @@ plates = [
             { 'STLs/Panels/midspan_panel_clip_4mm_x7.stl': 7 },
             { 'STLs/Panels/midspan_panel_clip_6mm_x8.stl': 8 },
         ],
-        output = "Plate/panels_sides.3mf"
+        output = "Plate/panels_sides.stl"
     ),
     dict(
         source = [
@@ -187,13 +181,13 @@ plates = [
             { 'STLs/Panels/handle_{a,b}_x2.stl': 2 },
             { 'STLs/Panels/door_hinge_x6.stl': 6 },
         ],
-        output = "Plate/panels_door.3mf"
+        output = "Plate/panels_door.stl"
     ),
     dict(
         source = [
             'STLs/Panels/exhaust_filter_{grill,housing}.stl',
         ],
-        output = "Plate/exhaust.3mf"
+        output = "Plate/exhaust.stl"
     ),
 
     ### Other
@@ -202,22 +196,22 @@ plates = [
             'STLs/Accessories/spool_holder.stl',
             'STLs/Accessories/BowdenHolder_Mod1.stl',
         ],
-        output = "Plate/accessories.3mf"
+        output = "Plate/accessories.stl"
     ),
     dict(
         source = [
             'STLs/Accessories/voron-bracket-revo-connect-6-mm-panel-m3-screw.3mf',
             'STLs/Accessories/*_Nozzle_Holder.stl',
         ],
-        output = "Plate/accessories_revo_nozzle_holder.3mf"
+        output = "Plate/accessories_revo_nozzle_holder.stl"
     ),
     dict(
         source = [{ 'STLs/Accessories/LED_Bar_Clip_Misumi_version2.stl': 29 }], # fits 29 behind Z belts on 350mm printer
-        output = "Plate/accessories_led_bar_clips.3mf"
+        output = "Plate/accessories_led_bar_clips.stl"
     ),
     dict(
         source = ['STLs/AngryCam/*.stl'],
-        output = "Plate/angry_cam.3mf",
+        output = "Plate/angry_cam.stl",
         split = True
     ),
     dict(
@@ -229,19 +223,19 @@ plates = [
             'STLs/Electronics/raspberry_pi_bracket.stl',
             'STLs/Electronics/rs25_psu_bracket.stl',
         ],
-        output = "Plate/electronics.3mf"
+        output = "Plate/electronics.stl"
     ),
     dict(
         source = ['STLs/Nevermore/V2_Duo_Plenum*.stl'],
-        output = "Plate/nevermore_plenum.3mf"
+        output = "Plate/nevermore_plenum.stl"
     ),
     dict(
         source = ['STLs/Nevermore/XL_Cartridge*.3mf'],
-        output = "Plate/nevermore_xl_cartridge.3mf"
+        output = "Plate/nevermore_xl_cartridge.stl"
     ),
     dict(
         source = ['STLs/Tools/*.stl', 'STLs/Tools/**/*.stl'],
-        output = "Plate/tools.3mf"
+        output = "Plate/tools.stl"
     ),
 ]
 
